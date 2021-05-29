a0e304813a1147fa2b0d97dcc0dac2a52898b106
Lucas Bustos <lbustos.silva@icloud.com>
2021-05-28 21:02:55 -0300

    Corrigndo ormconfig

--- src/server.ts
+++ src/server.ts
@@ -14,6 +14,6 @@ app.use(routes);
 const path = 'src/.env';
 dotenv.config({ path });
 
-app.listen(3000, () => {
-  console.log('Server started on port 3000!');
+app.listen(process.env.PORT || 3000, () => {
+  console.log(`Server started on port  ${process.env.PORT || 3000}!`);
 });
