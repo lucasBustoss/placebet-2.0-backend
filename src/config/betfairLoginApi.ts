1e379c68b5699665a0844fae6c8443bbb8992f4e
Lucas Bustos <lbustos.silva@icloud.com>
2021-07-25 21:22:20 -0300

    Implementação do login geral

--- src/config/betfairLoginApi.ts
+++ src/config/betfairLoginApi.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export default axios.create({
-  baseURL: 'http://localhost:3001',
-  // baseURL: 'https://betfair.placebet.com.br',
+  // baseURL: 'http://localhost:3001',
+  baseURL: 'https://betfair.placebet.com.br',
 });
